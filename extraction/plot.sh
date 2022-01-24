c5028f78b5a72e26ee0f56bdf87f504c66b63428
NaomiatLibrary <ray.oshikawa@gmail.com>
2022-01-24 13:13:11 +0900

    improve plot

--- extraction/plot.sh
+++ extraction/plot.sh
@@ -6,7 +6,9 @@ set style fill solid border lc rgb "black";
 set boxwidth 1;
 set yrange [0:];
 set title  "'$1'";
-set xtics font "Times New Roman,10";
+set title font "Times New Roman,12";
+set xtics font "Times New Roman,12";
+set xtics rotate;
 plot "./extraction/results_'$1'.csv" using ($0*4+0):2 with boxes lw 2 lc rgb "light-cyan" title "Precision",
      "./extraction/results_'$1'.csv" using ($0*4+0):2:5 with yerrorbars title "",
      "./extraction/results_'$1'.csv" using ($0*4+1):3:xtic(1) with boxes lw 2 lc rgb "light-green" title "Recall",
